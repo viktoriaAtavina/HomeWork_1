4ce65ca6c9ac26bb4c1b3d01a2d26c2a18918d25
Viktoria <viktoria.atavina@yandex.ru>
2022-10-19 23:04:05 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Является ли число а квадратом числа b?");
+
+int a = 5;
+int b = 25;
+
+if (b == a * a)
+
+{
+    Console.WriteLine ("Да, вляется");
+}
+else
+{
+    Console.WriteLine ("Нет, не является");
+}
---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("ВВедите номер дня недели");
+
+int namber = Convert.ToInt16 (Console.ReadLine());
+switch (namber)
+{
+    case 1 :Console.WriteLine ("Понедельник");
+    break;
+    case 2:Console.WriteLine ("Вторник");
+    break;
+    case 3:Console.WriteLine ("Среда");
+    break;
+    case 4:Console.WriteLine ("Четверг");
+    break;
+    case 5:Console.WriteLine ("Пятница");
+    break;
+    case 6:Console.WriteLine ("Суббота");
+    break;
+    case 7:Console.WriteLine ("Воскресенье");
+    break;
+}
